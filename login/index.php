0642e9feeb719a03020a05d398fb315c1299e0ef
cookiedragon234 <cookiedragon234@gmail.com>
2015-10-30 18:49:19 +0000

    Fixed form input names

--- login/index.php
+++ login/index.php
@@ -8,7 +8,7 @@
 		<form action="checklogin.php" method="post">
   			<span class="input-group-addon" id="basic-addon1">Username</span><br>
   			<input type="text" name="username" class="form-control" placeholder="Username" aria-describedby="basic-addon1"><br>
-			<span class="input-group-addon" id="basic-addon1">Username</span><br>
+			<span class="input-group-addon" id="basic-addon1">Password</span><br>
   			<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1"><br>
   			<input class="btn btn-default" type="submit" name="Submit">
   		</form>
