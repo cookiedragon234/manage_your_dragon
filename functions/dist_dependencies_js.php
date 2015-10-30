fe80dca809171ffb41e7e600b5aec25b80a81efe
cookiedragon234 <cookiedragon234@gmail.com>
2015-10-30 19:37:40 +0000

    Finished dist_dependencies_js

--- functions/dist_dependencies_js.php
+++ functions/dist_dependencies_js.php
@@ -1,3 +1,4 @@
 <?php
-	echo "<script src='../dist/jquery/jquery.min.js' type='text/javascript'>";
+	echo "<script src='../dist/jquery/jquery.min.js' type='text/javascript'></script>";
+	echo "<script src='../dist/bootstrap/js/bootstrap.min.js' type='text/javascript'></script>";
 ?>
