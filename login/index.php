bfe65e2d1a120df72f4dbb4f512f02ada4a9eda4
cookiedragon234 <cookiedragon234@gmail.com>
2015-10-30 18:40:21 +0000

    Started login page

---
+++ login/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MYD-Login</title>
+	</head>
+	<body>
+		<h1>Manage Your Dragon-Login</h1>
+		<form>
+  			<span class="input-group-addon" id="basic-addon1">Username</span><br>
+  			<input type="text" name="username" class="form-control" placeholder="Username" aria-describedby="basic-addon1"><br>
+			<span class="input-group-addon" id="basic-addon1">Username</span><br>
+  			<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1"><br>
+  			<input class="btn btn-default" type="submit" name="Submit">
+	</body>
+</html>
