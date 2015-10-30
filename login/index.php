313722f369578160fceaf3e52bd137f224824558
cookiedragon234 <cookiedragon234@gmail.com>
2015-10-30 18:44:39 +0000

    Fixed unfinished form

--- login/index.php
+++ login/index.php
@@ -5,11 +5,12 @@
 	</head>
 	<body>
 		<h1>Manage Your Dragon-Login</h1>
-		<form>
+		<form action="checklogin.php" method="post">
   			<span class="input-group-addon" id="basic-addon1">Username</span><br>
   			<input type="text" name="username" class="form-control" placeholder="Username" aria-describedby="basic-addon1"><br>
 			<span class="input-group-addon" id="basic-addon1">Username</span><br>
   			<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1"><br>
   			<input class="btn btn-default" type="submit" name="Submit">
+  		</form>
 	</body>
 </html>
