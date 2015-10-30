f44af0e5de3db83667e8aa2ad3b995a58f72d2cd
cookiedragon234 <cookiedragon234@gmail.com>
2015-10-30 18:52:53 +0000

    Started Signup

---
+++ signup/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MYD-Signup</title>
+	</head>
+	<body>
+		<h1>Manage Your Dragon-Signup</h1>
+		<form action="signupsend.php" method="post">
+			<span class="input-group-addon" id="basic-addon1">Name</span><br>
+  			<input type="text" name="name" class="form-control" placeholder="Name" aria-describedby="basic-addon1"><br>
+  			<span class="input-group-addon" id="basic-addon1">Email</span><br>
+  			<input type="text" name="email" class="form-control" placeholder="Email" aria-describedby="basic-addon1"><br>
+  			<span class="input-group-addon" id="basic-addon1">Username</span><br>
+  			<input type="text" name="username" class="form-control" placeholder="Username" aria-describedby="basic-addon1"><br>
+			<span class="input-group-addon" id="basic-addon1">Password</span><br>
+  			<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1"><br>
+  			<input class="btn btn-default" type="submit" name="Submit">
+  		</form>
+	</body>
+</html>
